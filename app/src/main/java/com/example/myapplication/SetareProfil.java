461eb4f1311120fc7fc3c18a2b58777139b4efda
alexaeliza <oana_eliza_alexa@yahoo.com>
2020-11-20 20:54:02 +0200

    eliza

--- app/src/main/java/com/example/myapplication/SetareProfil.java
+++ app/src/main/java/com/example/myapplication/SetareProfil.java
@@ -8,16 +8,16 @@ import android.view.View;
 import android.widget.Button;
 
 public class SetareProfil extends AppCompatActivity {
-    private Button button;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setare_profil);
 
-        button = findViewById(R.id.inapoi_inregistrare);
+        button1 = findViewById(R.id.inapoi_inregistrare);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SetareProfil.this, Inregistrare.class);
