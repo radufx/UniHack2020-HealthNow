7a1b9026daaf46a7129242a410262ca6e6bb4443
alexaeliza <oana_eliza_alexa@yahoo.com>
2020-11-20 20:49:27 +0200

    eliza

--- app/src/main/java/com/example/myapplication/Inregistrare.java
+++ app/src/main/java/com/example/myapplication/Inregistrare.java
@@ -2,13 +2,37 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inregistrare extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inregistrare);
+
+        button = findViewById(R.id.sign);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Inregistrare.this, SetareProfil.class);
+                startActivity(intent);
+            }
+        });
+
+        button = findViewById(R.id.inapoi_main_inregistrare);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Inregistrare.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/Inscriere.java
+++ app/src/main/java/com/example/myapplication/Inscriere.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inscriere extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscriere);
+
+        button = findViewById(R.id.inapoi_main_inscriere);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Inscriere.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
