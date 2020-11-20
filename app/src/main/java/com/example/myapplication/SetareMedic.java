11733889b9352db9ea9e782ef99d9645ae7d97b9
alexaeliza <oana_eliza_alexa@yahoo.com>
2020-11-20 23:10:30 +0200

    eliza

--- app/src/main/java/com/example/myapplication/SetareMedic.java
+++ app/src/main/java/com/example/myapplication/SetareMedic.java
@@ -6,25 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 
-public class Inscriere extends AppCompatActivity {
+public class SetareMedic extends AppCompatActivity {
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inscriere);
+        setContentView(R.layout.activity_setare_medic);
 
-        button = findViewById(R.id.inapoi_main_inscriere);
+        button = findViewById(R.id.inapoi_main_inregistrare);
 
         button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(Inscriere.this, MainActivity.class);
-                    startActivity(intent);
-                }
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SetareMedic.this, Inregistrare.class);
+                startActivity(intent);
+            }
         });
-
     }
 }
