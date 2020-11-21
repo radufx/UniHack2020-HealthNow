cc2a044d495d7d9f7ff239d24f9787355256f286
alexaeliza <oana_eliza_alexa@yahoo.com>
2020-11-21 13:00:22 +0200

    eliza

--- app/src/main/java/com/example/myapplication/Inscriere.java
+++ app/src/main/java/com/example/myapplication/Inscriere.java
@@ -6,14 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class Inscriere extends AppCompatActivity {
     private Button button;
-    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +37,7 @@ public class Inscriere extends AppCompatActivity {
             }
         });
 
-        mAuth = FirebaseAuth.getInstance();
+
 
     }
 
@@ -48,7 +45,7 @@ public class Inscriere extends AppCompatActivity {
     protected void onStart (){
         super.onStart();
 
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+
 
     }
 }
